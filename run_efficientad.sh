45e6352749de95064caed9a9e1f0e3925dd4b1df
Hesam <h.khanjani@blue-group.it>
2024-11-28 16:37:05 +0100

    add training pipeline without Autoencoder

--- run_efficientad.sh
+++ run_efficientad.sh
@@ -3,10 +3,9 @@
  /home/zf/Thesis/AnomalyDetection/venv/bin/python3.12 efficientad.py \
     --dataset mvtec_ad \
     --subdataset plastic_nut \
-    --output_dir output/plastic_nut_mini_imagenet_full_penalty0.75 \
+    --output_dir output/plastic_nut_mini_100_imagenet_full \
     --model_size medium \
     --weights models/teacher_medium.pth \
     --imagenet_train_path /home/zf/Thesis/Datasets/ImageNet \
-    --mvtec_ad_path  /home/zf/Thesis/Datasets/Real-IAD-mini \
-    --train_steps 70000 \
-    --coeff_penalty 0.75
+    --mvtec_ad_path  /home/zf/Thesis/Datasets/Real-IAD-mini-100 \
+    --train_steps 70000 
--- test_efficientad.sh
+++ test_efficientad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /home/zf/Thesis/AnomalyDetection/venv/bin/python3.12  mvtec_ad_evaluation/evaluate_experiment.py \
-    --dataset_base_dir '/home/zf/Thesis/Datasets/Real-IAD-mini' \
-    --anomaly_maps_dir './output/plastic_nut_mini_imagenet_full_penalty0.75/anomaly_maps/mvtec_ad/' \
-    --output_dir './output/plastic_nut_mini_imagenet_full_penalty0.75/metrics/mvtec_ad/' \
+    --dataset_base_dir '/home/zf/Thesis/Datasets/Real-IAD' \
+    --anomaly_maps_dir './output/plastic_nut_imagenet_full_NoAutoencoder/anomaly_maps/mvtec_ad/' \
+    --output_dir './output/plastic_nut_imagenet_full_NoAutoencoder/metrics/mvtec_ad/' \
     --evaluated_objects plastic_nut
